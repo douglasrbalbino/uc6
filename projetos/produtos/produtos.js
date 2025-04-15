f86b2e1ce56da5f6cad3c5429a81da127cf170c5
Douglas <doug.balbino@hotmail.com>
2025-04-15 20:12:19 -0300

    Continuei o projeto Produto em novo computador do senac

--- projetos/produtos/produtos.js
+++ projetos/produtos/produtos.js
@@ -7,9 +7,24 @@ if (produts.length === 0) {
     ul.innerHTML = '<li>Nenhum produto cadastrado!</li>';
 } else {
     produts.forEach(p => {
-        const li = document.createElement("li");
+        // cria e informa o texto dos itens da lista
+        const li = document.createElement("li");         
         li.textContent = p.toString();
 
+        // cria um botão "editar"
+        const btnEditar = document.createElement("button");
+        btnEditar.textContent = "Editar";
+
+        // cria um botão "excluir"
+        const btnExcluir = document.createElement("button");
+        btnExcluir.textContent = "Excluir";
+        btnExcluir.onclick = () => {
+            BancoDeDados.excluir(p.id);
+            window.location.reload();
+        }
+
+
+        li.append("   ", btnEditar, "   ", btnExcluir);
         ul.appendChild(li);
-    })
+    });
 }
